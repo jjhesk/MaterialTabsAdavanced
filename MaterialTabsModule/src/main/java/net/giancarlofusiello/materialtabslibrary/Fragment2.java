e41f5e0f23e8e5f076f180481d3fe913f0569253
Hesk <jobhesk@gmail.com>
2015-04-03 12:43:04 +0800

    added better UI controls
    UI control supports

---
+++ MaterialTabsModule/src/main/java/net/giancarlofusiello/materialtabslibrary/Fragment2.java
@@ -0,0 +1,30 @@
+package net.giancarlofusiello.materialtabslibrary;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment2 extends Fragment {
+
+
+    public Fragment2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment2, container, false);
+    }
+
+
+}
---
+++ MaterialTabsModule/src/main/java/net/giancarlofusiello/materialtabslibrary/MyAdapter.java
@@ -0,0 +1,45 @@
+package net.giancarlofusiello.materialtabslibrary;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by c766847 on 28/01/2015.
+ */
+public class MyAdapter extends FragmentStatePagerAdapter {
+
+    private static final int FRAGMENT_ONE = 0;
+    private static final int FRAGMENT_TWO = 1;
+    private static final int FRAGMENT_THREE = 2;
+
+    public MyAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case FRAGMENT_ONE : return new Fragment1();
+            case FRAGMENT_TWO : return new Fragment2();
+            case FRAGMENT_THREE : return new Fragment3();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case FRAGMENT_ONE : return "Frag1";
+            case FRAGMENT_TWO : return "Frag2";
+            case FRAGMENT_THREE : return "Frag3";
+        }
+        return super.getPageTitle(position);
+    }
+}
